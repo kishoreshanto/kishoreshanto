ff10f552c3aefdd6adf9a2a2e18efd0495a5a12b
Kishorè Shanto <tinykishore@gmail.com>
2023-05-14 01:13:19 +0600

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
-<main class="grid h-screen place-items-center">
+<main class="grid h-screen place-items-center dark:bg-zinc-800">
     <div class="flex flex-col gap-2 text-center">
-        <h1 class="text-3xl text-zinc-600"> Site under maintenance </h1>
-        <h1 class="text-xl font-light text-zinc-500"> Will be right back </h1>
+        <h1 class="text-3xl font-black text-zinc-600 dark:text-zinc-50"> Site under maintenance </h1>
+        <h1 class="text-xl font-light text-zinc-500 dark:text-gray-400"> Will be right back </h1>
     </div>
 </main>
