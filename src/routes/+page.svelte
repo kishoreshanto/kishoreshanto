3e3f44df94ab7d115ce899e467b61b67bcb1c117
Kishorè Shanto <tinykishore@gmail.com>
2023-05-14 01:11:32 +0600

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<main class="grid h-screen place-items-center">
+    <div class="flex flex-col gap-2 text-center">
+        <h1 class="text-3xl text-zinc-600"> Site under maintenance </h1>
+        <h1 class="text-xl font-light text-zinc-500"> Will be right back </h1>
+    </div>
+</main>
