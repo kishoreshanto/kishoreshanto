abb6f2ca73ef58bbbd09a3aa93bb973066167800
Kishorè Shanto <tinykishore@gmail.com>
2023-05-16 00:35:08 +0600

    Made Svelte Section

---
+++ src/components/Sidebar/NameSection.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let first_name, last_name;
+</script>
+
+<h1 class="select-none dark:text-white font-black text-5xl text-black">
+    {first_name}
+    <span class="text-sky-300">{last_name}</span>
+</h1>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
     import '../styles/global.css'
+    import Sidebar from "../components/Sidebar.svelte";
+    import TimeLine from "../components/TimeLine.svelte";
 </script>
 
-<slot class="antialiased"></slot>
+
+<main class="flex min-h-full flex-col bg-white dark:bg-gray-950">
+    <Sidebar/>
+
+    <div class="relative flex-auto">
+        <div class="pointer-events-none absolute inset-0 z-50 overflow-hidden lg:right-[calc(max(2rem,50%-38rem)+40rem)] lg:min-w-[32rem] lg:overflow-visible">
+            <TimeLine/>
+        </div>
+
+        <slot class="antialiased">
+            This is where timeline sits
+        </slot>
+    </div>
+
+</main>
