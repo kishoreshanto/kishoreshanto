86c577359fabfdc2f5e5d560b23e46905dce39f0
Kishorè Shanto <tinykishore@gmail.com>
2024-08-10 23:49:44 +0600

    Delete src/components directory

--- src/components/Sidebar/Signature.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import signature_black from "$lib/assets/signature-black.png"
-    import signature_white from "$lib/assets/signature-white.png"
-</script>
-
-<div >
-    <img src="{signature_black}" alt="verification signature" class="h-14 dark:hidden select-none">
-    <img src="{signature_white}" alt="verification signature" class="h-[4.2rem] hidden dark:block select-none -translate-x-2">
-</div>
