75c1471a5a3125fe30c44586472a714d52891111
Kishorè Shanto <tinykishore@icloud.com>
2024-08-12 02:31:45 +0600

    fixed GMT

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,14 +3,40 @@ export function isValidDate(dateString: string): boolean {
     return regex.test(dateString);
 }
 
+function makeGMTString(GMT: number): string {
+    let GMTString: string = 'GMT';
+    if (GMT >= 0) GMTString = GMTString + "+";
+    else GMTString = GMTString + "-";
+
+    // Add the hours, add a 0 if it's a single digit
+    if (Math.abs(GMT) < 10) GMTString = GMTString + "0";
+
+    // Add the hours, if it has a decimal, round it
+    const d: number = Math.floor(Math.abs(GMT));
+    const f: number = Math.abs(GMT) - d;
+    GMTString = GMTString + d;
+
+    // Add semicolon
+    GMTString = GMTString + ":";
+
+    // Add the minutes
+    const min: number = (f * 60);
+
+    // Add a 0 if it's a single digit
+    if (min < 10) GMTString = GMTString + "0";
+    GMTString = GMTString + min;
+
+    return GMTString;
+}
+
 export function getLocalTimeFromUTCOffset(utcOffset: number): { local_time: string, GMT: string } {
     /**
      * Get the current time in the local timezone
      * @param utcOffset - The offset from UTC
      * @returns - An array containing the 24-hour time and GMT value
      */
-    const now = new Date();
-    const utc = new Date(
+    const now: Date = new Date();
+    const utc: Date = new Date(
         now.getUTCFullYear(),
         now.getUTCMonth(),
         now.getUTCDate(),
@@ -23,11 +49,11 @@ export function getLocalTimeFromUTCOffset(utcOffset: number): { local_time: stri
     const offsetMilliseconds = utcOffset * 60 * 60 * 1000;
 
     // Apply the offset to get the local time
-    const new_date = new Date(utc.getTime() + offsetMilliseconds);
+    const new_date: Date = new Date(utc.getTime() + offsetMilliseconds);
 
     return {
         local_time: new_date.toString().split(' ')[4].split(':').slice(0, 2).join(':'),
-        GMT: new_date.toString().split(' ')[5]
+        GMT: makeGMTString(utcOffset)
     }
 }
 
