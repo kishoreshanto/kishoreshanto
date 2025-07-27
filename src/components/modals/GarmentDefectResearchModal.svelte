3e4dcd8c609d4730148cdd815f228f0699166b13
Kishore Shanto <tinykishore@icloud.com>
2025-07-27 12:40:15 +0600

    fix: Update class for gradient div to include flex layout in GarmentDefectResearchModal

--- src/components/modals/GarmentDefectResearchModal.svelte
+++ src/components/modals/GarmentDefectResearchModal.svelte
@@ -207,7 +207,7 @@
             </div>
         </div>
         
-        <div class="bg-gradient-to-r from-green-50 to-emerald-50 dark:from-green-900/20 dark:to-emerald-900/20 rounded-lg p-4 border border-green-200 dark:border-green-800">
+        <div class="bg-gradient-to-r flex from-green-50 to-emerald-50 dark:from-green-900/20 dark:to-emerald-900/20 rounded-lg p-4 border border-green-200 dark:border-green-800">
             <div class="flex items-center space-x-3">
                 <svg class="w-6 h-6 text-green-600 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20">
                     <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"/>
