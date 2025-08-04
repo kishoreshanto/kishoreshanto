1c5c9ca22f19cb72ff8737bb778ad3a4c7f7dddf
Kishore Shanto <tinykishore@icloud.com>
2025-08-05 02:33:55 +0600

    Refactor Language component styles for improved consistency and readability

--- src/components/sidebar/Language.svelte
+++ src/components/sidebar/Language.svelte
@@ -10,7 +10,8 @@
     <h1 class="mt-4 select-none cursor-default my-1 font-bold text-sky-800 dark:text-sky-600 font-mono">Languages</h1>
     <div class="select-none my-1 flex gap-x-2 gap-y-2 flex-wrap dark:text-white text-sm">
         {#each show_language as subject}
-            <h1 class="  px-3 py-0.5 rounded-2xl bg-gradient-to-r from-blue-50 to-indigo-100 dark:from-slate-800 dark:to-slate-700 text-center font-semibold cursor-default text-slate-700 dark:text-slate-200  transition-all duration-300 shadow-sm  border border-white/20 dark:border-slate-600/30 backdrop-blur-sm">{subject}</h1>
+            <h1 class=" bg-sky-100 text-center  font-semibold cursor-default text-zinc-600 hover:bg-sky-200 transition-all shadow
+        duration-300 dark:dark:hover:bg-sky-800 dark:text-zinc-200 dark:bg-sky-950  px-3 py-0.5 rounded-2xl">{subject}</h1>
         {/each}
     </div>
 {/if}
