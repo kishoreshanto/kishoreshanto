8cad08fd1aea1c35831b50941fb87c9a233bdf01
Kishore Shanto <tinykishore@icloud.com>
2026-03-17 01:04:46 +0600

    feat: add ContactBox and DisplayImage components to the hero page

---
+++ src/components/hero-page/DisplayImage.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import data from '$lib/data_en.json';
+
+	const image_url: string = data.display_image_url;
+	const alt_text: string = data.display_image_alt;
+</script>
+
+<img
+	src={image_url}
+	alt={alt_text}
+	class="h-36 w-36 rounded-full border-4 border-amber-700 shadow-lg lg:h-48 lg:w-48"
+/>
