d1e94f1fd6498e38f9a75910d879bc5fd3fc11db
Kishore Shanto <tinykishore@icloud.com>
2026-03-18 17:06:27 +0600

    refactor: enhance story page layout with story cards and styling

--- src/routes/story/+page.svelte
+++ src/routes/story/+page.svelte
@@ -1,2 +1,73 @@
-<h1 class="mb-105">Story</h1>
-<p>My story page — coming soon.</p>
+<script>
+	import TimeIcon from '$component/visuals/icons/TimeIcon.svelte';
+</script>
+
+<div
+	class="mx-auto mt-6 grid grid-cols-1 gap-6 md:grid-cols-2 md:px-16 lg:grid-cols-3 2xl:grid-cols-4"
+>
+	<!-- Card 1 -->
+	<div class="story-card">
+		<img
+			src="https://plus.unsplash.com/premium_photo-1673177667569-e3321a8d8256?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+			alt="for-Noir"
+		/>
+		<div class="story-card-body">
+			<h2 class="story-card-title">Meet Noir 🐈‍⬛</h2>
+			<p class="story-card-description">
+				Welcome Noir, my adorable black cat who has been a constant source of joy and companionship
+				in my life.
+			</p>
+			<div class="flex items-center align-middle">
+				<span>
+					<TimeIcon />
+				</span>
+
+				<p class="story-card-timestamp">{Date.now()}</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- Card 2 -->
+	<div class="story-card">
+		<img
+			src="https://plus.unsplash.com/premium_photo-1673177667569-e3321a8d8256?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+			alt="for-Noir"
+		/>
+		<div class="story-card-body">
+			<h2 class="story-card-title">Meet Noir 🐈‍⬛</h2>
+			<p class="story-card-description">
+				Welcome Noir, my adorable black cat who has been a constant source of joy and companionship
+				in my life.
+			</p>
+			<div class="flex items-center align-middle">
+				<span>
+					<TimeIcon />
+				</span>
+
+				<p class="story-card-timestamp">{Date.now()}</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- Card 3 -->
+	<div class="story-card">
+		<img
+			src="https://plus.unsplash.com/premium_photo-1673177667569-e3321a8d8256?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+			alt="for-Noir"
+		/>
+		<div class="story-card-body">
+			<h2 class="story-card-title">Meet Noir 🐈‍⬛</h2>
+			<p class="story-card-description">
+				Welcome Noir, my adorable black cat who has been a constant source of joy and companionship
+				in my life.
+			</p>
+			<div class="flex items-center align-middle">
+				<span>
+					<TimeIcon />
+				</span>
+
+				<p class="story-card-timestamp">{Date.now()}</p>
+			</div>
+		</div>
+	</div>
+</div>
