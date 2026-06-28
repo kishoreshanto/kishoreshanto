d93bb6bd9bcab22de2d387fe87e7cf54704d3ae7
Kishore Shanto <tinykishore@icloud.com>
2026-06-28 18:52:12 +0600

    Updated introsection

--- src/components/homepage/IntroSection.svelte
+++ src/components/homepage/IntroSection.svelte
@@ -1,6 +1,5 @@
 <script>
 	import { onMount } from 'svelte';
-	import ContactBox from './ContactBox.svelte';
 	import DisplayImage from './DisplayImage.svelte';
 
 	let animated = $state(false);
@@ -13,39 +12,31 @@
 	});
 </script>
 
-<div class="flex items-center justify-center xl:justify-start">
-	<div
-		class="flex flex-col items-center gap-4 text-center lg:gap-8 lg:text-start xl:flex-row 2xl:gap-18"
-	>
-		<div
-			class="flex flex-col items-center gap-4 text-center lg:flex-row lg:gap-8 lg:text-start 2xl:gap-18"
+<div class="flex flex-col-reverse items-center justify-center lg:flex-row lg:justify-between">
+	<div class="mt-4 flex flex-col gap-2 text-center lg:text-start">
+		<h1 class="font-crimson-text text-5xl lg:text-6xl 2xl:text-8xl">
+			Hey, <span class="name-highlight font-bold" class:name-highlight--animate={animated}
+				>Kishore</span
+			> here!
+		</h1>
+
+		<h3 class="font-crimson-text text-2xl font-semibold md:text-3xl xl:text-4xl">
+			Software and AI Engineer, Researcher
+		</h3>
+
+		<p
+			class="lg mt-2 flex flex-col gap-3 text-center font-lora text-lg text-gray-700 lg:max-w-xl lg:text-justify xl:max-w-4xl"
 		>
-			<DisplayImage />
-
-			<div class="flex flex-col gap-2">
-				<h1 class="font-crimson-text text-5xl lg:text-6xl 2xl:text-8xl">
-					Hey, <span class="name-highlight font-bold" class:name-highlight--animate={animated}
-						>Kishore</span
-					> here!
-				</h1>
-
-				<h3 class="font-crimson-text text-2xl font-semibold md:text-3xl xl:text-4xl">
-					Software and AI Engineer, Researcher
-				</h3>
-
-				<p
-					class="lg mt-2 flex flex-col gap-3 text-center font-lora text-lg text-gray-700 lg:max-w-xl lg:text-justify xl:max-w-4xl"
-				>
-					<span
-						>Software and AI engineer here. I love building beautiful and functional softwares and
-						training AI systems that solve real problems. I also enjoy writing about my experiences
-						and sharing my knowledge with others.</span
-					>
-					<em>I believe in open source, open knowledge, and the power of community.</em>
-				</p>
-			</div>
-		</div>
-
-		<ContactBox />
+			<span
+				>Software and AI engineer here. I love building beautiful and functional softwares and
+				training AI systems that solve real problems. I also enjoy writing about my experiences and
+				sharing my knowledge with others.</span
+			>
+			<em>I believe in open source, open knowledge, and the power of community.</em>
+		</p>
 	</div>
+	<DisplayImage />
+	<!-- </div> -->
+
+	<!-- <ContactBox /> -->
 </div>
