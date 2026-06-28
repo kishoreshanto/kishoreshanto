26a834c667610bca590f37f435731310bd815647
Kishore Shanto <tinykishore@icloud.com>
2026-06-28 13:42:19 +0600

    icon update

---
+++ src/components/shared/svg/GoogleScholarIcon.svelte
@@ -0,0 +1,11 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+	width="22px"
+	height="22px"
+	class="translate-y-[0.050rem] fill-amber-700"
+>
+	<path
+		d="M12 3 1.5 8.25 12 13.5l8.56-4.28v6.53H22V8.25zm-6 8.7v3.54c0 1.62 2.69 2.94 6 2.94s6-1.32 6-2.94V11.7L12 15z"
+	/>
+</svg>
