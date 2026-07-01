acef0ce07e6b7c7d88172640f807b94930114d11
Kishore Shanto <tinykishore@icloud.com>
2026-07-01 10:07:43 +0600

    Timeline Component of academic page

---
+++ src/components/academic/AcademicDisplayImage.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import data from '$lib/data/personal.json';
+
+	const image_url: string = data.display_image_url;
+	const alt_text: string = data.display_image_alt;
+</script>
+
+<img
+	src={image_url}
+	alt={alt_text}
+	width="864"
+	height="864"
+	decoding="async"
+	fetchpriority="high"
+	class="h-22 w-22 rounded-full border-4 border-amber-700 shadow-lg md:h-44 md:w-44"
+/>
