afb300eaee86c78749002fd67403db6c48022f22
Kishore Shanto <tinykishore@icloud.com>
2026-07-01 10:06:35 +0600

    Temporary Redirection

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,10 @@
+import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
+
+export const load: LayoutServerLoad = ({ url }) => {
+	if (url.pathname === '/') {
+		redirect(308, '/academic');
+	}
+
+	return {};
+};
